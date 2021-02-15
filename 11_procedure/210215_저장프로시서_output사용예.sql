27bb1a6d2c729fc70714f6f6da48ad7f8ef38611
hugoMGSung <personar95@naver.com>
2021-02-15 15:14:56 +0900

    ㅈㄷㄹㅈㄷㄹ

--- 11_procedure/210215_저장프로시서_output사용예.sql
+++ 11_procedure/210215_저장프로시서_output사용예.sql
@@ -13,3 +13,19 @@ declare @newResult int;
 set @newResult = 0;
 exec plusNew 3, 4, @newResult output;
 print concat('PlusNew(3, 4) = ', @newResult);
+
+
+/*
+C#에서는 아래와 같음
+static void Main(string[] args)
+{
+    int newResult;
+    plusNew(3, 4, out newResult);
+    Console.WriteLine($"plusNew(3, 4) = {newResult}");
+}
+
+private static void plusNew(int p1, int p2, out int pout)
+{
+    pout = p1 + p2;
+}
+*/
