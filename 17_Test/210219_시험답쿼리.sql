b0ba97943b56cd7a1f58726dc7c0ad8c92a4d164
hugoMGSung <personar95@naver.com>
2021-02-19 11:49:00 +0900

    서브쿼리, 통계 처리 다른방법

--- 17_Test/210219_시험답쿼리.sql
+++ 17_Test/210219_시험답쿼리.sql
@@ -14,28 +14,64 @@ select top(10)
   from memberTBL;
 
  -- 2-2
-select Idx, concat('제목 : ', Names) as Names,
-       concat('저자 > ', Author) as Author,
-	   format(ReleaseDate, 'yyyy년 MM월 dd일') as '출판일',
-	   ISBN, format(Price, '#,#원') as '가격'
+select Idx
+     , concat('제목 : ', Names) as Names
+	 , concat('저자 > ', Author) as Author
+	 , format(ReleaseDate, 'yyyy년 MM월 dd일') as '출판일'
+	 , ISBN
+	 , format(Price, '#,#원') as '가격'
   from bookstbl
  order by Idx desc;
  
- -- 3- 1
-select b.idx as '번호', b.division as '장르번호',
-	   d.Names as '장르',
-       b.names as '책제목', b.author as '저자'
+ -- 3-1 문제
+select b.idx as '번호'
+     , b.division as '장르번호'
+	 , d.Names as '장르'
+	 , b.names as '책제목'
+	 , b.author as '저자'
   from bookstbl as b
  inner join divtbl as d
     on b.Division = d.Division
  where b.Division = 'B002';
 
--- 3-2
-select m.Names, m.Levels, m.Addr, r.rentalDate
+-- 3-1 문제 다르게 푸는 방법1
+select b.idx 번호
+     , b.division '장르 번호'
+	 , d.Names 장르
+	 , b.names 책제목
+	 , b.author 저자
+  from bookstbl b
+     , divtbl d
+ where b.Division = d.Division
+   and b.Division = 'B002';
+
+-- 3-1 문제 다르게 푸는 방법2
+select b.idx as '번호'
+     , b.division as '장르번호'
+	 --, d.Names as '장르'
+	 , (select Names from divtbl where Division = b.Division) as 장르
+	 , b.names as '책제목'
+	 , b.author as '저자'
+  from bookstbl as b
+ where b.Division = 'B002';
+
+-- 3-2 문제
+select m.Names
+     , m.Levels
+	 , m.Addr
+	 , r.rentalDate
   from membertbl as m
   left outer join rentaltbl as r
     on m.Idx = r.memberIdx
  where r.rentalDate is null;
+
+-- 3-2 다르게 풀기
+select m.Names
+     , m.Levels
+	 , m.Addr
+	 , null as rentalDate
+  from membertbl as m
+ where m.Idx not in (select memberIdx from rentaltbl as r);
 
 -- 4-1
 select * from divTbl;
@@ -53,3 +89,16 @@ select d.names, sum(b.price) as '총합계금액'
  inner join divtbl as d
     on b.Division = d.Division
  group by rollup(d.names);
+
+-- 5번 다르게 풀기
+select (select names from divtbl where Division = a.Division) as names
+		, a.sum_price as '총합계금액'
+	from  
+			(select Division, sum(price) as sum_price
+				from bookstbl
+				group by Division) as a
+union all
+select null, sum(price)
+ from booksTbl
+
+
